9f9b31ca66043065e0cf411ac102d9ab7a3b7fa4
abhirimal <abhirimal009@gmail.com>
2022-10-22 18:21:59 +0545

    complete theory of inheritance, Q.N.1 to Q.N.8

---
+++ src/oop/inheritance/InheritanceTheory.java
@@ -0,0 +1,52 @@
+/*
+    1. What is inheritance ?
+    Ans: Inheritance is a process by which a child class acquires the properties of a parent class.
+         In OOP, it provides the idea of reusability i.e. we can add additional features to an
+         existing class without modifying it.
+         The new child class will have the combined features of both the classes i.e. parent class and
+         its own.
+    2. Why do we need inheritance ?
+    Ans: We need inheritance for code reusability. It also helps us to achieve method overriding.
+
+    3. What are the types of inheritance and which types of inheritance is supported in java ?
+    Ans: There are 5 types of inheritance. They are:
+    i) Single inheritance               ii) Multiple Inheritance
+    iii) Multilevel Inheritance         iv) Hierarchical Inheritance
+    v) Hybrid Inheritance
+
+    All types of multiple inheritance is supported in Java. However, it doesn't support multiple
+    inheritance in classes. We can implement multiple inheritance by using interface.
+
+    4. Explain all the inheritance types in java.
+    i) Single inheritance
+    Ans: A single child class is derived by a single parent class.
+
+    ii) Multiple Inheritance
+    Ans:A single child class is derived by two or more parent class.
+
+    iii) Multilevel Inheritance
+    Ans: In this type of inheritance, a derived class is created from another derived class.
+         It should contain atleast 3 levels.
+
+    iv) Hierarchical Inheritance
+    Ans: Multiple child classes are dervied from a single parent class.
+
+    v) Hybrid Inheritance
+    Ans: It is formed by the combination of two or more inheritances.
+
+    5. Can we extend more than one class in java ?
+    Ans: Yes, we can extend more than one class in java.
+
+    6. What is the use of super keywords ?
+    Ans: The super keyword in Java is a reference variable which is used to refer immediate parent
+         class object.
+
+    7. Can  we access the subclass method using superclass objects ?
+    Ans: No it is not possible. A superclass has no knowledge of its subclasses.
+
+    8. Why is multiple inheritance not supported in java ?
+    Ans: Multiple Inheritance is not supported in java because it leads to a diamond problem.
+         We can implement multiple inheritance by using interface.
+
+
+ */
