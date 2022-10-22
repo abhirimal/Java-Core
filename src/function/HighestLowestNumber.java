d477a08235fbecb230716759fe1a9344f9529b06
abhirimal <abhirimal009@gmail.com>
2022-10-22 17:19:15 +0545

    complete Q.N.7 of fucntion

--- src/function/HighestLowestNumber.java
+++ src/function/HighestLowestNumber.java
@@ -8,40 +8,36 @@ package function;
 public class HighestLowestNumber {
 
     //declaring a method averageNumber
-    public void averageNumber(int num1, int num2, int num3){
+    public void averageNumber(int num1, int num2, int num3) {
 
         // condition for highest number
-        if(num1>num2 && num1>num3){
-            System.out.println("The highest number is "+num1);
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("The highest number is " + num1);
 
-        }
-        else if(num2>num1 && num2>num3){
-            System.out.println("The highest number is "+num2);
-        }
-        else{
-            System.out.println("The highest number is "+num3);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("The highest number is " + num2);
+        } else {
+            System.out.println("The highest number is " + num3);
 
         }
 
         //condition for lowest number
-        if(num1<num2 && num1<num3){
-            System.out.println("The lowest number is "+num1);
-        }
-        else if(num2<num3 && num2<num1){
-            System.out.println("The lowest number is "+num2);
+        if (num1 < num2 && num1 < num3) {
+            System.out.println("The lowest number is " + num1);
+        } else if (num2 < num3 && num2 < num1) {
+            System.out.println("The lowest number is " + num2);
 
-        }
-        else{
-            System.out.println("The lowest number is "+num3);
+        } else {
+            System.out.println("The lowest number is " + num3);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //creating a object of HighestLowestNumber class
         HighestLowestNumber obj = new HighestLowestNumber();
 
         //calling a method averageNumber from object obj
-        obj.averageNumber(18,5,12);
+        obj.averageNumber(18, 5, 12);
     }
 }
