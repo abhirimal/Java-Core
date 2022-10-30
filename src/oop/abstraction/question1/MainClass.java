29cfed4acfdc3718002d3fe0a0cbe3f5990dc179
abhirimal <abhirimal009@gmail.com>
2022-10-30 11:12:37 +0545

    complete Q.N.1 of abstraction

---
+++ src/oop/abstraction/question1/MainClass.java
@@ -0,0 +1,13 @@
+package oop.abstraction.question1;
+
+public class MainClass {
+
+    public static void main(String[] args){
+
+        FirstSubclass first = new FirstSubclass();
+        first.message();
+
+        SecondSubclass second = new SecondSubclass();
+        second.message();
+    }
+}
---
+++ src/oop/abstraction/question1/SecondSubclass.java
@@ -0,0 +1,9 @@
+package oop.abstraction.question1;
+
+public class SecondSubclass extends Parent{
+
+    @Override
+    public void message() {
+        System.out.println("This is second subclass.");
+    }
+}
