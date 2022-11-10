bedf433050782505442620354c3d1d17684891cf
abhirimal <abhirimal009@gmail.com>
2022-11-11 01:11:23 +0545

    complete Q.N.2 of static keyword

---
+++ src/static_keyword/Time.java
@@ -0,0 +1,14 @@
+/*
+    2. What will be the output of the following program.
+
+    public class Time {
+        int a = 50;
+        int b = 10;
+        public static void main(String args[]) {
+            a += b--;
+            System.out.println(a);
+    }
+
+    Ans : There will be compilation error because we are trying to access the
+          variables of class from a main method without creating objects.
+ */
