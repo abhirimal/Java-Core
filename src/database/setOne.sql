3535135c18f07d18141c4931dee33b05d9dc58bc
abhirimal <abhirimal009@gmail.com>
2022-11-19 15:14:12 +0545

    complete Q.N.2 of database Set One

--- src/database/setOne.sql
+++ src/database/setOne.sql
@@ -2,3 +2,9 @@
     1. Display details of jobs where the minimum salary is greater than 10000.
  */
         select * from jobs where min_salary>10000;
+
+/*
+    2.	Display the first name and join date of the employees who joined between 2002 and 2005.
+ */
+        select first_name, hire_date from employees
+        where hire_date between '2002-01-01' and '2005-01-01';
