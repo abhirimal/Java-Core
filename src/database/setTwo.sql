bb37f43baa220f658943a0e8d74b2e0ee12b775c
abhirimal <abhirimal009@gmail.com>
2022-11-20 02:42:17 +0545

    complete Q.N.3 of database Set Two

--- src/database/setTwo.sql
+++ src/database/setTwo.sql
@@ -11,3 +11,12 @@
         select first_name,datediff(end_date,start_date) /365
             AS experience from employees
                 JOIN job_history on employees.employee_id=job_history.employee_id ;
+
+/*
+    3.	Display manager ID and number of employees managed by the manager.
+ */
+
+        select departments.manager_id, count(employees.manager_id)  As numberOfEmployees
+            from departments
+                join employees on departments.manager_id=employees.manager_id
+                    group by departments.manager_id ;
